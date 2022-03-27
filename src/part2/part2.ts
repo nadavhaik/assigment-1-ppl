93ce6af5f89253a40a2b29813488d9586b91f6ed
nadavhaik <67607457+nadavhaik@users.noreply.github.com>
2022-03-27 15:02:23 +0300

    Update part2.ts

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -1,9 +1,15 @@
 import * as R from "ramda";
+import {filter} from "ramda";
 
 const stringToArray = R.split("");
 
+const isEnglishChar: (s: string) => boolean = (s: string) => {
+    return s.length == 1 && ((56 <= s.charCodeAt(0) <= 90) || (97 <= s.charCodeAt(0) <= 122))
+}
+
 /* Question 1 */
-export const countLetters: (s: string) => {} = undefined
+export const countLetters: (s: string) => {} = R.pipe(stringToArray, filter(isEnglishChar), R.countBy(R.toLower));
+
 
 /* Question 2 */
 export const isPaired: (s: string) => boolean = undefined
