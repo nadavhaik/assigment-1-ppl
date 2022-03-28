a645351097356057eae8f09b3a232ab2346b0989
nadavhaik <67607457+nadavhaik@users.noreply.github.com>
2022-03-28 11:56:19 +0300

    נראה לי שסיימתי את חלק 2

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -1,24 +1,23 @@
 import * as R from "ramda";
-import {filter} from "ramda";
 
 const stringToArray = R.split("");
-const arrayToString = (x: string[]) => R.reduce((s1, s2: string) => s1+s2, "", x);
+const arrayToString = (strings: string[]) => R.reduce((s1, s2: string) => s1+s2, "", strings)
 
 /* Question 1 */
 const isEnglishChar: (s: string) => boolean = (s: string) =>
     s.length == 1 && ((56 <= s.charCodeAt(0) && s.charCodeAt(0) <= 90) ||
         (97 <= s.charCodeAt(0) && s.charCodeAt(0) <= 122))
 
-export const countLetters: (s: string) => {} = R.pipe(stringToArray, filter(isEnglishChar), R.countBy(R.toLower))
+export const countLetters: (s: string) => {} = R.pipe(stringToArray, R.filter(isEnglishChar), R.countBy(R.toLower))
 
 const filterPairs: (s: string, leftBracket: string, rightBracket: string) => string[] =
     (s: string, leftBracket: string, rightBracket: string) =>
         R.pipe(stringToArray, R.filter((c) => c == leftBracket || c == rightBracket))(s)
 
 /* Question 2 */
-const specificIsPaired: (s: string, leftBracket: string, rightBracket: string) => boolean = (s: string, leftBracket: string, rightBracket: string) =>
+const specificIsPaired: (s: string, leftBracket: string, rightBracket: string) =>
+    boolean = (s: string, leftBracket: string, rightBracket: string) =>
         leftBracket.length == 1 && rightBracket.length == 1 &&
-            filterPairs(s, leftBracket, rightBracket).length % 2 == 0 &&
             arrayToString(filterPairs(s, leftBracket, rightBracket)) ===
             (leftBracket + rightBracket).repeat(filterPairs(s, leftBracket, rightBracket).length/2)
 
@@ -26,9 +25,13 @@ export const isPaired: (s: string) => boolean = (s: string) => specificIsPaired(
     && specificIsPaired(s, "[", "]") && specificIsPaired(s, "{", "}")
 
 /* Question 3 */
-interface WordTree {
+export interface WordTree {
     root: string;
     children: WordTree[];
 }
 
-export const treeToSentence = (t: WordTree): string => undefined
+const recTreeToSentence: (x: WordTree) => string = (t: WordTree) =>
+    t.root + " " + arrayToString(R.map(recTreeToSentence, t.children))
+const deleteLastChar = (s: string) => s.substring(0, s.length-1)
+
+export const treeToSentence: (t: WordTree) => string = R.pipe(recTreeToSentence, deleteLastChar)
