b896b8e479a33740baabe7064b81ae79ec3716a2
nadavhaik <67607457+nadavhaik@users.noreply.github.com>
2022-03-31 14:19:48 +0300

    תיקון שאלה 2 חלק 2

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -1,6 +1,8 @@
 import * as R from "ramda";
 
 const stringToArray = R.split("");
+const arrayToString: (arr: string[]) => string = (arr: string[]) => arr.join('')
+const deleteLastChar: (s: string) =>  string = (s: string) => s.substring(0, s.length-1)
 
 /* Question 1 */
 const isEnglishChar: (s: string) => boolean = (s: string) =>
@@ -10,24 +12,32 @@ const isEnglishChar: (s: string) => boolean = (s: string) =>
 export const countLetters: (s: string) => {} = R.pipe(stringToArray, R.filter(isEnglishChar), R.countBy(R.toLower))
 
 /* Question 2 */
-const numberOfOccurrences: (s: string, char: string) => number =
-    (s: string, char: string) => stringToArray(s).filter(R.equals(char)).length
-const prefixAt = (s: string) => (i: number) => s.substring(0, i)
-const allPrefixes: (s: string) => string[] =
-    (s: string) => R.map(prefixAt(s), R.range(0, s.length+1))
-const validPrefix = (leftBracket: string, rightBracket: string) =>
-    (prefix: string) =>
-        numberOfOccurrences(prefix, leftBracket) >= numberOfOccurrences(prefix, rightBracket)
+const bracketCloses: (c1: string, c2: string) => boolean = (opener: string, closer: string) =>
+    (opener === "{" && closer === "}") || (opener === "(" && closer === ")") || (opener === "[" && closer === "]")
+const isOpeningBracket: (c: string) => boolean = (c: string) =>
+    c === "{" || c === "[" || c === "("
+const isClosingBracket: (c: string) => boolean = (c: string) =>
+    c === "}" || c === "]" || c === ")"
+const isBracket: (c: string) => boolean = (c: string) => isOpeningBracket(c) || isClosingBracket(c)
 
-const specificIsPaired: (s: string, leftBracket: string, rightBracket: string) => boolean =
-    (s: string, leftBracket: string, rightBracket: string) =>
-        numberOfOccurrences(s, leftBracket) === numberOfOccurrences(s, rightBracket) &&
-        R.all(validPrefix(leftBracket, rightBracket), allPrefixes(s))
+const deleteFirstChar: (s: string) => string = (s: string) => s.substring(1)
+const firstChar: (s: string) => string = (s: string) => s.charAt(0)
+const lastChar: (s: string) => string = (s: string) => s.charAt(s.length-1)
+const addStrings: (s1: string, s2: string) => string = (s1: string, s2: string) => s1 + s2
+
+export const recIsPaired: (bracketsString: string, stackString: string) => boolean =
+    (bracketsString: string, stackString: string) =>
+        (bracketsString === "") ? stackString === "" :
+        isOpeningBracket(firstChar(bracketsString)) ?
+            recIsPaired(deleteFirstChar(bracketsString), addStrings(stackString, firstChar(bracketsString))) :
+        /** else: bracketsString starts with a closing bracket **/
+        stackString === "" || !bracketCloses(lastChar(stackString), firstChar(bracketsString)) ? false :
+        // ** else: brackets do close **/
+        recIsPaired(deleteFirstChar(bracketsString), deleteLastChar(stackString))
+
 
 export const isPaired: (s: string) => boolean = (s: string) =>
-    specificIsPaired(s, "(", ")") &&
-    specificIsPaired(s, "[", "]") &&
-    specificIsPaired(s, "{", "}")
+    recIsPaired(arrayToString(stringToArray(s).filter(isBracket)), "")
 
 /* Question 3 */
 export interface WordTree {
@@ -35,8 +45,7 @@ export interface WordTree {
     children: WordTree[];
 }
 
-const deleteLastChar: (s: string) =>  string = (s: string) => s.substring(0, s.length-1)
 const recTreeToSentence: (t: WordTree) => string = (t: WordTree) =>
-    t.root + " " + R.map(recTreeToSentence, t.children).join('')
+    t.root + " " + arrayToString(R.map(recTreeToSentence, t.children))
 
 export const treeToSentence: (t: WordTree) => string = R.pipe(recTreeToSentence, deleteLastChar)
